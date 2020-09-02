f90acc57b4aa806ac73382f8d47e1f2e941b79b8
kenny mais <kennymais@gmail.com>
2020-09-02 10:14:00 -0500

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require("express");
-var app = express();
-var server = require("http").createServer(app);
-var io = require("socket.io").listen(server);
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
 const https = require("https");
 
 users = [];
 connections = [];
 
-server.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 console.log("server running");
 
 /* 
